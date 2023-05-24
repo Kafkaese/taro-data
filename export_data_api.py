13aefa72693e06d20c8a58ec7363c827b46892d6
Frederik Laubisch <kafkaese@gmail.com>
2023-05-24 11:54:28 +0400

    fix data

--- export_data_api.py
+++ export_data_api.py
@@ -3,15 +3,18 @@ import pandas as pd
 
 api = FastAPI()
 
-data = pd.read_csv('data/total_exports.csv')
+data = pd.read_csv('data/total_exports.csv',  index_col=0)
 
 @api.get("/")
 async def root():
     return {'status': 200}
 
-@api.get("/total{country_name}")
+@api.get("/total")
 async def total(country_name):
+    #print(country_name)
+    #print(data.loc[country_name, 'Value'])
+    
     try:
-        return {'value': data[country_name, 'Value']}
+        return {'value': str(data.loc[country_name, 'Value'])}
     except:
         return {'value': 'no data'}
