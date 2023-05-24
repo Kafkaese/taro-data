9638853ea54c8cad8f80eefcbe92fc00414be760
Frederik Laubisch <kafkaese@gmail.com>
2023-05-24 12:21:56 +0400

    add cors middleware

--- export_data_api.py
+++ export_data_api.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 import pandas as pd
 
 api = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 data = pd.read_csv('data/total_exports.csv',  index_col=0)
 
