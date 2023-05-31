0a7c82d3ad1f60a363056210129b64aa554708cd
Frederik Laubisch <kafkaese@gmail.com>
2023-05-31 10:49:59 +0400

    postgres dest wip + docstring

--- taro/pipeline.py
+++ taro/pipeline.py
@@ -1,13 +1,31 @@
 import psycopg2
 from taro.scraper import democracy_index
 
-def democracy_index(source: str = 'scraper', dest: str = 'postgres') -> None:
+def democracy_index(source: str = 'scraper', dest: str = 'postgres', **kwargs) -> None:
+    '''
+    Gets democracy index data from source and writes to dest.
     
+    Keyword arguments:
+    source -- either one of ('scraper', 'csv')
+    dest -- either one of ('postgres', 'csv')
+    
+    db_conn -- postgres database connection. Only if dest = 'postgres'
+    csv_src_path -- path to source csv file. Only if source = 'csv'
+    csv_dest_path -- path to destination csv. Only if dest = 'csv'
+
+    '''    
     # Run scraper
     if source == 'scraper':
         data = democracy_index()
 
     if dest == 'csv':
         data.to_csv('../data/dem_id_TEST.csv')
+    
     elif dest == 'postgres':
+        if 'db_con' not in kwargs.keys():
+            raise TypeError("If dest = 'postgres' is passed, keyword arguement db_conn is required")
+        
+        db_conn = kwargs['db_conn']
+
+
         pass
