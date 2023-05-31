52bb728f4a12cfce98c5f5cd2ceb722e1034ead4
Frederik Laubisch <kafkaese@gmail.com>
2023-05-31 12:03:01 +0400

    fix writing to db

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -3,7 +3,7 @@ from taro.pipeline import democracy_index_pipeline
 from sqlalchemy import create_engine
 import psycopg2
 
-conn_string = 'postgresql://postgres:password@127.0.0.1/postgres'
+conn_string = 'postgresql://postgres:password@localhost/postgres'
 
 db = create_engine(conn_string)
 conn = db.connect()
@@ -14,3 +14,10 @@ conn1 = psycopg2.connect(
     host='127.0.0.1', 
     port= '5432'
 )
+
+def test_democracy_index_pipeline():
+    
+    democracy_index_pipeline(source='scraper', dest='postgres', db_conn = conn)
+    
+if __name__ == '__main__':
+    test_democracy_index_pipeline()
