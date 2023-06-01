3ffb690cf10aec9a8037770ad3342a782d9aaa49
Frederik Laubisch <kafkaese@gmail.com>
2023-06-01 10:19:15 +0400

    fix peace index dataframe

--- taro/pipeline.py
+++ taro/pipeline.py
@@ -1,6 +1,7 @@
 import psycopg2
 from taro.scraper import democracy_index_scraper
 import pandas as pd
+import os
 
 def democracy_index_pipeline(source: str = 'scraper', dest: str = 'postgres', **kwargs) -> bool:
     '''
@@ -37,7 +38,21 @@ def democracy_index_pipeline(source: str = 'scraper', dest: str = 'postgres', **
         pass
     
 def peace_index_pipe(source: str = 'csv', dest: str = 'postgres', **kwargs) -> bool:
+    '''
+    Gets peace index data from 'source' and writes to 'dest'
     
+    Keyword arguments:
+    source -- either one of ('csv')
+    dest -- either one of ('postgres', 'csv')
+    
+    db_conn -- postgres database connection. Only if dest = 'postgres'
+    csv_src_path -- path to source csv file. Only if source = 'csv'
+    csv_dest_path -- path to destination csv. Only if dest = 'csv'
+
+    Returns:
+    0 if data was successfully written to dest, 1 if not
+    
+    '''
     
     if source == 'csv':
         if 'csv_path' not in kwargs.keys():
@@ -48,7 +63,7 @@ def peace_index_pipe(source: str = 'csv', dest: str = 'postgres', **kwargs) -> b
         peace_df = pd.read_csv(csv_path, header=3)
         
         # replace with corresponding pipe
-        codes = pd.read_csv('../data/countries_info.csv', index_col=0)
+        codes = pd.read_csv(os.path.join(os.path.dirname(__file__),'../data/countries_info.csv'), index_col=0)
         
         ''' 
         Gets dataframe with peace index values for years 2008 - 2022
@@ -57,5 +72,17 @@ def peace_index_pipe(source: str = 'csv', dest: str = 'postgres', **kwargs) -> b
         -  drop remaining unneeded columns
         - set iso 2 country code as index
         '''
-        peace_index_df = peace_df.merge(codes, left_on='iso3c', right_on='Alpha-3 code').iloc[:,:21].drop(columns = ['iso3c', 'Country','Country name', 'Official state name', 'Sovereignty']).set_index('Alpha-2 code')
-    
+        peace_df = peace_df.merge(codes, left_on='iso3c', right_on='Alpha-3 code').loc[:,['Alpha-2 code', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016',
+       '2017', '2018', '2019', '2020', '2021', '2022']].set_index('Alpha-2 code')
+        
+    if dest == 'postgres':
+        
+        if 'db_conn' not in kwargs.keys():
+            raise TypeError("If dest = 'postgres' is passed, keyword arguement db_conn is required")
+        
+        db_conn = kwargs['db_conn']
+
+        peace_df.to_sql('peace_index', db_conn, if_exists='replace')
+
+    else:
+        pass
