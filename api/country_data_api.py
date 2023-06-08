a987e7f6e1db1f2eb37e65158ec28d3b7cecaff5
Frederik Laubisch <kafkaese@gmail.com>
2023-06-08 20:03:00 +0200

    add year endpoints skeleton

--- api/country_data_api.py
+++ api/country_data_api.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import psycopg2
+from sqlalchemy import create_engine
 
 import pandas as pd
 
@@ -19,6 +21,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# database connection
+conn_string = 'postgresql://postgres:password@localhost/postgres'
+db = create_engine(conn_string)
+conn = db.connect()
+conn1 = psycopg2.connect(
+    database="postgres",
+    user='postgres', 
+    password='password', 
+    host='127.0.0.1', 
+    port= '5432'
+)
+cursor = conn1.cursor()
+
 # metadata
 metadata = pd.read_csv('data/metadata.csv',  index_col=0)
 democracy_index_data = pd.read_csv('data/democracy_index.csv',  index_col=0)
@@ -36,6 +51,7 @@ total_imports_data = pd.read_csv('data/total_imports.csv',  index_col=0)
 
 @app.get("/")
 async def root():
+    
     return {'status': 200}
 
 
@@ -84,6 +100,15 @@ async def exports_total(country_code):
     except:
         return {'value': 'no data'}
 
+@app.get("/exports/year")
+async def exports_total(country_code, year):
+    #print(country_name)
+    #print(data.loc[country_name, 'Value'])
+    
+    try:
+        return {'value': str(export_total_data.loc[country_code, 'Value'])}
+    except:
+        return {'value': 'no data'}
 
 # import path endpoints
 
