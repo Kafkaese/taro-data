5fa4d095b5307ce3789778c9ac720b4106061be8
Frederik Laubisch <kafkaese@gmail.com>
2023-07-06 15:12:43 +0200

    set firewall rule

--- terraform/test.tf
+++ terraform/test.tf
@@ -20,3 +20,11 @@ resource "azurerm_postgresql_flexible_server_database" "pg-db" {
   charset = "UTF8"
   collation = "en_US.utf8"
 }
+
+resource "azurerm_postgresql_firewall_rule" "pg-server-open" {
+  name                = "all public"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_postgresql_flexible_server.pg-server.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
+}
