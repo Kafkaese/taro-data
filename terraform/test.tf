200c2dc5c247c9d04dcfd8fa267da30301647ec9
Frederik Laubisch <kafkaese@gmail.com>
2023-07-06 19:25:36 +0200

    use random id suffix for server name to prevent name already in use error

--- terraform/test.tf
+++ terraform/test.tf
@@ -1,10 +1,14 @@
+resource "random_id" "pg-server-id" {
+    byte_length = 8
+    prefix = "taro-staging-server"
+}  
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = var.resource_group_name
 }
 
 resource "azurerm_postgresql_flexible_server" "pg-server" {
-  name = var.postgres_server
+  name = "${lower(random_id.storage_account.hex)}"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku_name = "B_Standard_B1ms"
