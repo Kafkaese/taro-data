574d6e30cfc01f3beb0dcf56ac2f549964c3d427
Frederik Laubisch <kafkaese@gmail.com>
2023-07-06 13:22:32 +0200

    add postgres variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,17 @@ variable "resource_group_name" {
   default     = "taro-test-env"
   description = "The resource group name."
 }
+
+variable "postgres_user" {
+  default = "postgres"
+  sensitive = true
+}
+
+variable "postgres_password" {
+  default = "secret"
+  sensitive = true
+}
+
+variable "postgres_database" {
+  default = "taro-db"
+}
