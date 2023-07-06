3498211bf7cf47b8a84fe24356509db272c21340
Frederik Laubisch <kafkaese@gmail.com>
2023-07-06 13:23:40 +0200

    add postgres server and db

--- terraform/test.tf
+++ terraform/test.tf
@@ -2,3 +2,21 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = var.resource_group_name
 }
+
+resource "azurerm_postgresql_flexible_server" "pg-server" {
+  name = "taro-server"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name = "B_Standard_B1ms"
+  storage_mb = 32768
+  version = 11
+  administrator_login = var.postgres_user
+  administrator_password = var.postgres_password
+}
+
+resource "azurerm_postgresql_flexible_server_database" "pg-db" {
+  name = var.postgres_database
+  server_id = azurerm_postgresql_flexible_server.pg-server.id
+  charset = "UTF8"
+  collation = "en_US.utf8"
+}
