6032c6bdea387bdf87c3f32890bc8817444b78fa
Frederik Laubisch <kafkaese@gmail.com>
2023-07-10 11:48:09 +0200

    add tests for country name endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,9 +4,32 @@ import os
 API_HOST = os.environ['API_HOST']
 API_PORT = os.environ['API_PORT']
 
+# test for root endpoint 
 def test_root_endpoint_status_code():
     URL = f'http://{API_HOST}:{API_PORT}/'
     
     response = requests.get(URL)
-    print(response.status_code)
+
     assert response.status_code == 200
+
+# tests for counry name endpoint
+def test_metadata_name_short_endpoint_status_code():
+    ENDPOINT = '/metadata/name/short'
+    URL = f'http://{API_HOST}:{API_PORT}{ENDPOINT}'
+    
+    response = requests.get(URL)
+    
+    assert response.status_code == 422
+    
+def test_metadata_name_short_request_status_code():
+    ENDPOINT = '/metadata/name/short'
+    URL = f'http://{API_HOST}:{API_PORT}{ENDPOINT}'
+    
+    PARAMS = {
+        "country_code": "CA" 
+    }
+    
+    response = requests.get(URL, params=PARAMS)
+    
+    assert response.status_code == 501
+    
