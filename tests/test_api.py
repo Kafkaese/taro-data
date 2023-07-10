0d5530d806bdbbeefe56b35960d00dbdcb01815e
Frederik Laubisch <kafkaese@gmail.com>
2023-07-10 12:09:57 +0200

    add test for peace index endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -67,3 +67,38 @@ def test_metadata_democracy_index_request():
     response = requests.get(URL, params=PARAMS)
     
     assert response.json()['value'] == 9.24
+    
+# test "/metadata/peace_index" endpoint
+def test_metadata_peace_index_endpoint_status_code():
+    ENDPOINT = "/metadata/peace_index"
+    URL = f'http://{API_HOST}:{API_PORT}{ENDPOINT}'
+    
+    response = requests.get(URL)
+    
+    assert response.status_code == 422
+    
+def test_metadata_peace_index_request_status_code():
+    ENDPOINT = "/metadata/peace_index"
+    URL = f'http://{API_HOST}:{API_PORT}{ENDPOINT}'
+    
+    PARAMS = {
+        "country_code": "CA", 
+        "year": 2020
+    }
+    
+    response = requests.get(URL, params=PARAMS)
+    
+    assert response.status_code == 200
+
+def test_metadata_peace_index_request():
+    ENDPOINT = "/metadata/peace_index"
+    URL = f'http://{API_HOST}:{API_PORT}{ENDPOINT}'
+    
+    PARAMS = {
+        "country_code": "CA", 
+        "year": 2020
+    }
+    
+    response = requests.get(URL, params=PARAMS)
+    
+    assert response.json()['value'] == 1.333
