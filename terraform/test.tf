863ef726e195d9f11a8e87ddf07e089db10d0a05
Frederik Laubisch <kafkaese@gmail.com>
2023-07-10 10:34:51 +0200

    fix random id resource

--- terraform/test.tf
+++ terraform/test.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_postgresql_flexible_server" "pg-server" {
-  name = "${lower(random_id.storage_account.hex)}"
+  name = "${lower(random_id.pg-server-id.hex)}"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku_name = "B_Standard_B1ms"
