6867d72d418d2ad8ba449b86d62f6c467ee2380f
Frederik Laubisch <kafkaese@gmail.com>
2023-07-14 10:21:10 +0200

    comment out outputs of deactivated resources

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,15 @@
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
-
-output "server_name" {
-  value = azurerm_postgresql_flexible_server.pg-server.name
+/*
+output "acr_service_principal_app_id" {
+  value = azurerm_container_registry.taro-registry.identity[0].principal_id
 }
+
+output "acr_service_principal_password" {
+  value = azurerm_container_registry.taro-registry.identity[0].passwords[0].value
+}
+*/
+#output "server_name" {
+#  value = azurerm_postgresql_flexible_server.pg-server.name
+#}
