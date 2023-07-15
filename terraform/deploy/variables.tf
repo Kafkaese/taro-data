42dd72535482a60059644c7daf1fc73d12b4dd47
Frederik Laubisch <kafkaese@gmail.com>
2023-07-15 09:42:47 +0200

    add variables for acr credentials in tf

--- terraform/deploy/variables.tf
+++ terraform/deploy/variables.tf
@@ -7,3 +7,11 @@ variable "resource_group_name" {
   default     = "taro-test-env"
   description = "The resource group name."
 }
+
+variable "image_registry_credential_user" {
+  default = "user"
+}
+
+variable "image_registry_credential_password" {
+  default = "secret"
+}
